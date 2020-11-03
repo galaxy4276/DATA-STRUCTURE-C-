ad78144c014ff6bf7b3c084abaa5b3b68a863a07
galaxy4276 <galaxyhi4276@gmail.com>
2020-11-04 00:59:41 +0900

    선형 탐색 간단 구현

--- Linear-Search.c
+++ Linear-Search.c
@@ -1,10 +1,34 @@
 #include<stdio.h>
 
 
+int LSearch(int ar[], int len, int target) {
+  int i;
+  for (i = 0; i < len; i++) {
+    if (ar[i] == target) {
+      return i;
+    }
+  }
+  return -1; // Not Found
+}
+
 int main() {
-  printf("hello world!\n");
-  int x;
-  scanf("%d", &x);
-  printf("%d\n", x);
-  return 0;
+  int arr[] = {3, 5, 2, 4, 9};
+  int idx;
+
+  printf("size: %d\n", sizeof(arr)/sizeof(int));
+  idx = LSearch(arr, sizeof(arr)/sizeof(int), 4);
+
+  if (idx == -1) {
+    printf("Å½»ö ½ÇÆĞ\n");
+  } else {
+    printf("Å¸°Ù ÀúÀå ÀÎµ¦½º: %d \n", idx);
+  }
+  
+  idx = LSearch(arr, sizeof(arr)/sizeof(int), 7);
+
+  if (idx == -1) {
+    printf("Å½»ö ½ÇÆĞ\n");
+  } else {
+    printf("Å¸°Ù ÀúÀå ÀÎµ¦½º: %d \n", idx);
+  }
 } 
