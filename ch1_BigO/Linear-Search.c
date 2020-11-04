e202a2da672ef8d20d26d8f0b49d1c11582335ac
galaxy4276 <galaxyhi4276@gmail.com>
2020-11-04 16:37:40 +0900

    ch1: BigO에 대한 이해와 계산

---
+++ ch1_BigO/Linear-Search.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+
+
+int LSearch(int ar[], int len, int target) {
+  int i;
+  for (i = 0; i < len; i++) {
+    if (ar[i] == target) {
+      return i;
+    }
+  }
+  return -1; // Not Found
+}
+
+int main() {
+  int arr[] = {3, 5, 2, 4, 9};
+  int idx;
+
+  printf("size: %d\n", sizeof(arr)/sizeof(int));
+  idx = LSearch(arr, sizeof(arr)/sizeof(int), 4);
+
+  if (idx == -1) {
+    printf("??? ????\n");
+  } else {
+    printf("??? ???? ?ех???: %d \n", idx);
+  }
+  
+  idx = LSearch(arr, sizeof(arr)/sizeof(int), 7);
+
+  if (idx == -1) {
+    printf("??? ????\n");
+  } else {                                                                                                                                                                 
+    printf("??? ???? ?ех???: %d \n", idx);
+  }
+} 
